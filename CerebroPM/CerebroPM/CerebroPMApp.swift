8c661ee67f3786bcae7148cc35d6d9abc91ea1d0
Sherwin Espela <sherwin.espela@gmail.com>
2024-05-20 14:49:12 -0700

    Adding Xcode Project

---
+++ CerebroPM/CerebroPM/CerebroPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerebroPMApp.swift
+//  CerebroPM
+//
+//  Created by Sherwin Espela on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct CerebroPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CerebroPM/CerebroPM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CerebroPM
+//
+//  Created by Sherwin Espela on 2024-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
